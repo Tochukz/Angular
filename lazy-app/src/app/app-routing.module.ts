90c9660a8d88913fc8ed3b4614d739aa8d86779b
Chucks Nwachukwu <chucks@bbd.co.za>
2021-11-27 18:21:47 +0200

    Added Module Routing

--- lazy-app/src/app/app-routing.module.ts
+++ lazy-app/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules, PreloadingStrategy } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'customer', 
+    loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) 
+  },
+  {
+    path: 'orders',
+    loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule),
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes, 
+      {
+        // preloadingStrategy: PreloadAllModules
+      }
+    )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
